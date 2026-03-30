6a821449f77a05ef0c87a4e2b704e8d963885641
Tanvir K <90351492+TanvirK41@users.noreply.github.com>
2026-03-30 14:22:11 -0400

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+
+# ✅ NEW CONNECTION (replaces get_active_session)
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 # Title and instructions
 st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
